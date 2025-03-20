e8d91ded17ef3ed659f44ed1fbc33be8e4ea75ba
Shubham Chaudhary <shubhamchaudharybst8953@gmail.com>
2025-03-20 21:36:19 +0530

    few changes in some files

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+
 process.noDeprecation = true; // disable deprecation warnings
 
 const express = require("express");
--- routes/listing.js
+++ routes/listing.js
@@ -4,15 +4,23 @@ const wrapAsync = require("../utils/wrapAsync.js");
 const Listing = require("../models/listing.js");
 const { isLoggedIn, isOwner, validateListing } = require("../middleware.js");
 const listingsController = require("../controllers/listings.js");
+const multer  = require('multer')
+const upload = multer({ dest: 'uploads/' })
 
 router
   .route("/")
   .get(listingsController.index)
-  .post(
-    isLoggedIn,
-    validateListing,
-    wrapAsync(listingsController.createListing)
-  );
+  // .post(
+  //   isLoggedIn,
+  //   validateListing,
+  //   wrapAsync(listingsController.createListing)
+  // );
+  .post( upload.single('listing[imageUrl]'),(req, res) => {
+    res.send(req.file);
+  console.log(req.file);
+  });
+    
+      
 
 // new route
 router.get("/new", isLoggedIn, listingsController.renderNewForm);
